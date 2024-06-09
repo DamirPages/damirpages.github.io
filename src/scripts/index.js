29b74d26f10e63816b844e1839ce3ace54071a5b
DamirPages <123557281+DamirPages@users.noreply.github.com>
2024-06-09 10:37:23 +0500

    Add files via upload

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,7 +2,96 @@ import '@/styles/index.scss';
 import 'swiper/css';
 import 'nice-select2/src/scss/nice-select2.scss';
 
+import { Fancybox } from '@fancyapps/ui';
+import '@fancyapps/ui/dist/fancybox/fancybox.css';
+
+Fancybox.bind('[data-fancybox]');
+
 const selects = document.querySelectorAll('.selectize');
 selects.forEach(select => {
 	NiceSelect.bind(select);
+});
+
+const dropdowns = document.querySelectorAll('.menu__dropdown-button');
+dropdowns.forEach(dropdown => {
+	dropdown.addEventListener('click', () => {
+		if (dropdown.classList.contains('active')) {
+			dropdown.classList.remove('active');
+			dropdown.nextElementSibling.style.height = 0;
+		} else {
+			dropdown.classList.add('active');
+			const height = dropdown.nextElementSibling.scrollHeight;
+			dropdown.nextElementSibling.style.height = height + 'px';
+		}
+	});
+});
+
+const testButtons = document.querySelectorAll('.test-button');
+let questionPassed = 0;
+const totalQuestions = testButtons.length;
+if (testButtons.length > 0) {
+	document.querySelector('.progress__total-question').textContent = testButtons.length;
+}
+function editProgreess() {
+	const percent = (questionPassed / totalQuestions) * 100;
+	document.querySelector('.progress__percent').textContent = Math.round(percent) + '%';
+	document.querySelector('.progress__current-question').textContent = questionPassed + 1;
+	document.querySelector('.progress__bar-line').style.width = percent + '%';
+}
+
+testButtons.forEach(button => {
+	button.addEventListener('click', () => {
+		questionPassed++;
+		button.closest('.test__block ').classList.remove('active');
+		button.closest('.test__block ').nextElementSibling?.classList.add('active');
+		editProgreess();
+	});
+	button
+		.closest('.test__block ')
+		.querySelectorAll('input')
+		.forEach(input => {
+			input.addEventListener(
+				'change',
+				() => {
+					button.classList.remove('disabled');
+				},
+				{ once: true }
+			);
+		});
+});
+
+document.modalOpen = modal => {
+	document.modalClose();
+	document.querySelector(modal).closest('.modal').classList.add('active');
+	const bodyWidth = document.body.clientWidth;
+	document.body.style.width = bodyWidth + 'px';
+	document.body.classList.add('no-scroll');
+};
+
+document.modalClose = () => {
+	document.querySelectorAll('.modal').forEach(modal => modal.classList.remove('active'));
+	document.body.classList.remove('no-scroll');
+	document.body.style.width = 'auto';
+};
+
+const modalOperBtns = document.querySelectorAll('[data-modal-open]');
+const modalCloseBtns = document.querySelectorAll('[data-modal-close]');
+const modalElements = document.querySelectorAll('.modal');
+modalOperBtns.forEach(button => {
+	button.addEventListener('click', () => {
+		const modal = button.dataset.modalOpen;
+		document.modalOpen(modal);
+	});
+});
+modalCloseBtns.forEach(button => {
+	button.addEventListener('click', () => {
+		document.modalClose();
+	});
+});
+modalElements.forEach(modal => {
+	modal.addEventListener('click', e => {
+		if (e.target === modal) {
+			document.modalClose();
+		}
+	});
 });
